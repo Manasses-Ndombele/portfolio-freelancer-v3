1cc86a7fce37280c1704e377d1a3ced1b2e7df96
Manasses Ndombele <manassesndombelefreelancer@gmail.com>
2025-01-08 01:40:29 +0100

    removed completely autoprefixer, added logic to close the modal and added styles of bg of select and btn toggle theme to light mode

--- web/src/components/ModalProject/index.tsx
+++ web/src/components/ModalProject/index.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, MouseEvent } from "react";
 import Link from "next/link";
 import { useProject } from "@/context/ProjectContext";
 import { FaGithub, FaGlobe } from "react-icons/fa";
@@ -7,7 +7,7 @@ import "@/styles/components/modal-project.scss";
 
 export default function ModalProject() {
   const { project } = useProject();
-  const [modalStatus, setModalStatus] = useState("closed");
+  const [modalStatus, setModalStatus] = useState<string>("closed");
   useEffect(() => {
     if (
       project.projectTitle !== "" &&
@@ -19,12 +19,28 @@ export default function ModalProject() {
     }
   }, [project]);
 
+  const closeModal = (): void => {
+    setModalStatus("closed");
+  };
+
+  const handleOverlayClick = (e: MouseEvent<HTMLDivElement>): void => {
+    if ((e.target as HTMLElement).id === "modal-overlay") {
+      closeModal();
+    }
+  };
+
   return (
     <div
       id="modal-overlay"
       className={modalStatus}
+      onClick={handleOverlayClick}
     >
-      <div id="modal-project">
+      <div
+        id="modal-project"
+        onClick={(e: MouseEvent<HTMLDivElement>): void => {
+          e.stopPropagation();
+        }}
+      >
         <h3>{project.projectTitle}</h3>
         <p>{project.projectDescription}</p>
         <div id="modal-actions">
